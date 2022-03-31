acca374313551d37177a727e5714dbde8953ebfb
Yhonathan Pavan <yhonathannpavan@gmail.com>
2022-03-31 00:04:45 -0300

    Adicionado bean do ModelMapper

--- API-Partidos-Politicos/avaliacao/src/main/java/com/spring/avaliacao/AvaliacaoApplication.java
+++ API-Partidos-Politicos/avaliacao/src/main/java/com/spring/avaliacao/AvaliacaoApplication.java
@@ -1,10 +1,17 @@
 package com.spring.avaliacao;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AvaliacaoApplication {
+
+	@Bean
+	public ModelMapper mapper(){
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AvaliacaoApplication.class, args);
