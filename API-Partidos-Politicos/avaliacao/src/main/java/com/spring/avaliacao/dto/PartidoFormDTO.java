ab20605d182acb221c02b4657f58a5d89d3d4072
Yhonathan Pavan <yhonathannpavan@gmail.com>
2022-03-31 17:30:42 -0300

    Adicionado anotações de validação

--- API-Partidos-Politicos/avaliacao/src/main/java/com/spring/avaliacao/dto/PartidoFormDTO.java
+++ API-Partidos-Politicos/avaliacao/src/main/java/com/spring/avaliacao/dto/PartidoFormDTO.java
@@ -5,14 +5,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class PartidoFormDTO {
+    @NotNull @NotEmpty
     private String nome;
+    @NotNull @NotEmpty
     private String sigla;
+    @NotNull
     private Ideologia ideologia;
+    @NotEmpty
     private LocalDate dataDeFundacao;
 }
