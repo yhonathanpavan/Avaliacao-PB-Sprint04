6e719ce00fd87620cc8283054b489e2b12b6388b
Yhonathan Pavan <yhonathannpavan@gmail.com>
2022-03-31 00:05:57 -0300

    Adicionado novos partidos e associados

--- API-Partidos-Politicos/avaliacao/src/main/resources/data.sql
+++ API-Partidos-Politicos/avaliacao/src/main/resources/data.sql
@@ -1,3 +1,14 @@
-INSERT INTO PARTIDO(nome, sigla, ideologia, data_de_fundacao) VALUES('Partido Trabalhista', 'PT', 'ESQUERDA', '2001-12-01');
+INSERT INTO PARTIDO(nome, sigla, ideologia, data_de_fundacao) VALUES('Movimento Democrático Brasileiro', 'MDB', 'CENTRO', '1980-01-15');
+INSERT INTO PARTIDO(nome, sigla, ideologia, data_de_fundacao) VALUES('Partido dos Trabalhadores', 'PT', 'ESQUERDA', '1980-02-10');
+INSERT INTO PARTIDO(nome, sigla, ideologia, data_de_fundacao) VALUES('Partido da Social Democracia Brasileira', 'PSDB', 'CENTRO', '1988-06-25');
+INSERT INTO PARTIDO(nome, sigla, ideologia, data_de_fundacao) VALUES('Progressistas', 'PP', 'DIREITA', '1995-08-08');
+INSERT INTO PARTIDO(nome, sigla, ideologia, data_de_fundacao) VALUES('Partido Democrático Trabalhista', 'PDT', 'ESQUERDA', '1979-06-17');
+INSERT INTO PARTIDO(nome, sigla, ideologia, data_de_fundacao) VALUES('Partido Social Cristão', 'PSC', 'DIREITA', '1985-05-15');
 
-INSERT INTO ASSOCIADO(nome, cargo_politico, data_nascimento, sexo) VALUES('Lula', 'PRESIDENTE', '2001-12-1', 'MASCULINO');
+INSERT INTO ASSOCIADO(nome, cargo_politico, data_nascimento, sexo) VALUES('Baleia Rossi', 'PRESIDENTE', '1972-06-9', 'MASCULINO');
+INSERT INTO ASSOCIADO(nome, cargo_politico, data_nascimento, sexo) VALUES('Adriana', 'PREFEITO', '1964-10-12', 'FEMININO');
+INSERT INTO ASSOCIADO(nome, cargo_politico, data_nascimento, sexo) VALUES('João Dória', 'GOVERNADOR', '1957-12-16', 'MASCULINO');
+INSERT INTO ASSOCIADO(nome, cargo_politico, data_nascimento, sexo) VALUES('Mailza Gomes', 'SENADOR', '1976-12-10', 'FEMININO');
+INSERT INTO ASSOCIADO(nome, cargo_politico, data_nascimento, sexo) VALUES('Alex Marco Santana', 'DEPUTADO_FEDERAL', '1972-06-22', 'MASCULINO');
+INSERT INTO ASSOCIADO(nome, cargo_politico, data_nascimento, sexo) VALUES('Fátima Nunes', 'DEPUTADO_ESTADUAL', '1953-03-11', 'FEMININO');
+INSERT INTO ASSOCIADO(nome, cargo_politico, data_nascimento, sexo) VALUES('Airton Barbosa', 'VEREADOR', '1955-12-27', 'MASCULINO');
