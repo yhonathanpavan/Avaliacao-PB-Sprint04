44fc34ebfb46f82d016d4ec03258b9efed9ec4dc
Yhonathan Pavan <yhonathannpavan@gmail.com>
2022-03-31 00:06:44 -0300

    Adicionado atributos, getters, setters e construtores

--- API-Partidos-Politicos/avaliacao/src/main/java/com/spring/avaliacao/dto/AssociadoFormDTO.java
+++ API-Partidos-Politicos/avaliacao/src/main/java/com/spring/avaliacao/dto/AssociadoFormDTO.java
@@ -1,4 +1,20 @@
 package com.spring.avaliacao.dto;
 
+import com.spring.avaliacao.constants.CargoPolitico;
+import com.spring.avaliacao.constants.Sexo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AssociadoFormDTO {
+
+    private String nome;
+    private CargoPolitico cargoPolitico;
+    private Date dataNascimento;
+    private Sexo sexo;
 }
--- API-Partidos-Politicos/avaliacao/src/main/java/com/spring/avaliacao/dto/PartidoFormDTO.java
+++ API-Partidos-Politicos/avaliacao/src/main/java/com/spring/avaliacao/dto/PartidoFormDTO.java
@@ -1,4 +1,18 @@
 package com.spring.avaliacao.dto;
 
+import com.spring.avaliacao.constants.Ideologia;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PartidoFormDTO {
+    private String nome;
+    private String sigla;
+    private Ideologia ideologia;
+    private Date dataDeFundacao;
 }
