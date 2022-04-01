ced4254123fcbfc19571e7fcc22e9e49ff6d4565
Yhonathan Pavan <yhonathannpavan@gmail.com>
2022-04-01 19:41:54 -0300

    Removendo notação NotEmpty

--- API-Partidos-Politicos/avaliacao/src/main/java/com/spring/avaliacao/dto/VincularFormDTO.java
+++ API-Partidos-Politicos/avaliacao/src/main/java/com/spring/avaliacao/dto/VincularFormDTO.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Data
@@ -12,9 +11,9 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor
 public class VincularFormDTO{
 
-    @NotNull @NotEmpty
+    @NotNull
     private Long idAssociado;
-    @NotNull @NotEmpty
+    @NotNull
     private Long idPartido;
 
 }
