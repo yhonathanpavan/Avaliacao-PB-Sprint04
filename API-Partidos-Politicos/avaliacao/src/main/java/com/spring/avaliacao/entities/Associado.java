7e4e7d5708a1cd00d863cac96ee8b8cafd629395
Yhonathan Pavan <yhonathannpavan@gmail.com>
2022-04-01 19:41:14 -0300

    Removendo imports desnecessários

--- API-Partidos-Politicos/avaliacao/src/main/java/com/spring/avaliacao/entities/Associado.java
+++ API-Partidos-Politicos/avaliacao/src/main/java/com/spring/avaliacao/entities/Associado.java
@@ -1,19 +1,13 @@
 package com.spring.avaliacao.entities;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.spring.avaliacao.constants.CargoPolitico;
 import com.spring.avaliacao.constants.Sexo;
-import com.spring.avaliacao.serializer.LocalDateSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.util.Date;
 
 @Data
 @AllArgsConstructor
