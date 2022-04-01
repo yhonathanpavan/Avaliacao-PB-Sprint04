ad1d49d2c444de47f121f8861b6c9aa9b9bba46f
Yhonathan Pavan <yhonathannpavan@gmail.com>
2022-04-01 17:06:17 -0300

    Adicionando validação nos atributos

--- API-Partidos-Politicos/avaliacao/src/main/java/com/spring/avaliacao/dto/VincularFormDTO.java
+++ API-Partidos-Politicos/avaliacao/src/main/java/com/spring/avaliacao/dto/VincularFormDTO.java
@@ -4,12 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class VincularFormDTO{
 
+    @NotNull @NotEmpty
     private Long idAssociado;
+    @NotNull @NotEmpty
     private Long idPartido;
 
 }
